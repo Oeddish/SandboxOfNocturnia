0af72e049c6bf3c0a65bad526da1dd6f790b472e
Oed <oedwak@comcast.net>
2025-08-11 15:33:18 -0700

    Check-in for sandbox before crazy things from Matt!

---
+++ Content/Items/Items/SliceOfCakeDrinkable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FragmentsOfNocturnia.Content.Items.Items
+{
+    internal class SliceOfCakeDrinkable : GlobalItem
+    {
+        public override bool CanUseItem(Item item, Player player)
+        {
+            // Check if it's the vanilla Slice of Cake
+            if (item.type == ItemID.SliceOfCake)
+            {
+                // If Alt is pressed, allow the vanilla item use (placement)
+                if (Keyboard.GetState().IsKeyDown(Keys.LeftAlt) || Keyboard.GetState().IsKeyDown(Keys.RightAlt))
+                {
+                    return base.CanUseItem(item, player);
+                }
+
+                // Add the buff instead of placing item
+                player.AddBuff(BuffID.SugarRush, 120 * 60); // 2 minutes in ticks
+
+                // Play a sound effect
+                Terraria.Audio.SoundEngine.PlaySound(SoundID.Item3, player.position); 
+
+                // Return false to prevent the vanilla item use (placing it)
+                return false;
+            }
+
+            // For all other items, allow vanilla behavior
+            return base.CanUseItem(item, player);
+        }
+    }
+}
---
+++ Content/Items/Weapons/Thrown/NightmareBomb.cs
@@ -0,0 +1,29 @@
+using FragmentsOfNocturnia.Content.Items.Items;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FragmentsOfNocturnia.Content.Items.Weapons.Thrown
+{
+    public class NightmareBomb : ModItem
+    {
+        public override string Texture => "Terraria/Images/Item_" + ItemID.ScarabBomb;
+        private static readonly int COUNT_RECIPE_RESULTS = 24;
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.Bomb);
+            Item.shoot = ModContent.ProjectileType<Projectiles.Thrown.NightmareBombProjectile>();
+            Item.value = Item.buyPrice(silver: 50);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe(COUNT_RECIPE_RESULTS);
+            recipe.AddIngredient(ItemID.Bomb, 1); // Base bomb
+            recipe.AddIngredient(ModContent.ItemType<BatEssence>(), 1);
+            recipe.AddTile(TileID.Anvils); // Crafting station
+            recipe.Register();
+        }
+    }
+}
